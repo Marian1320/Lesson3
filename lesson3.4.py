8208e41071d6bdfca6aeba1c5eba344587801893
Mariana <marian.zaqarian@gmail.com>
2025-01-26 16:15:06 +0400

    Add Lesson 3 files

---
+++ lesson3.4.py
@@ -0,0 +1,6 @@
+#4.	Write a Python program to change a given string to a new
+# string where the first and last charshave been exchanged.
+x='abcdefgh'
+y='h'+x[1:-1]+'a'
+print(y)
+
---
+++ lesson3.7.py
@@ -0,0 +1,13 @@
+#7. Write a Python program to print the floating numbers upto 2 decimal places
+# (number must be not greater than 10)
+
+
+x=2.145548
+if x<=10: x=f"{x:.2f}"
+print(x)
+
+x=9.5748
+if x<=10: x=f"{x:.2f}"
+print(x)
+
+# այս առաջադրանքի լուծումը ԱԲ-ով եմ նայել, այդքան էլ չեմ հասկացել
